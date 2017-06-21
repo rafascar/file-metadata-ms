e40a9558c1676c7bf5f9e62592fb15eb37b5d47e
System <none>
2017-06-21 22:52:55 +0000

    Implement /filesize

---
+++ README.md
@@ -0,0 +1,5 @@
+File Metadata Microservice
+==========================
+freeCodeCamp API Project
+
+https://disco-icicle.glitch.me/
--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 
 // init project
 var express = require('express');
+var multer = require('multer');
+
 var app = express();
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -14,6 +18,11 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/filesize", upload.single('upl'), function (request, response) {
+  var file = request.file ? {size: request.file.size} : {};
+  response.send(file);
 });
 
 // listen for requests :)
